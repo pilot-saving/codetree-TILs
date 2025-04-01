ba0127e635574c40bf4e8ce5061d08af0ec51323
Woo Yeonseo <106312178+pilot-saving@users.noreply.github.com>
2025-04-02 07:58:12 +0900

    Title: 씨 오 더블유 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250402/씨 오 더블유 2/c-o-w-2.cpp
+++ 250402/씨 오 더블유 2/c-o-w-2.cpp
@@ -13,9 +13,11 @@ int main() {
     // Please write your code here.
     int cnt = 0;
     for (int i=0; i<n-2; i++) {
+        if (str[i] != 'C') continue;
         for (int j=i+1; j<n-1; j++) {
-            for (int k=i+2; k<n; k++) {
-                if (str[i] == 'C' && str[j] == 'O' && str[k] == 'W') cnt++;
+            if (str[j] != 'O') continue;
+            for (int k=j+1; k<n; k++) {
+                if (str[k] == 'W') cnt++;
             }
         }
     }
