3ed142263f0756c432f0c1c3e15846a565f4bac3
Woo Yeonseo <106312178+pilot-saving@users.noreply.github.com>
2025-04-02 07:53:31 +0900

    Title: 씨 오 더블유 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250402/씨 오 더블유 2/c-o-w-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int n;
+string str;
+
+int main() {
+    cin >> n;
+    cin >> str;
+
+    // Please write your code here.
+    int cnt = 0;
+    for (int i=0; i<n-2; i++) {
+        for (int j=1; j<n-1; j++) {
+            for (int k=2; k<n; k++) {
+                if (str[i] == 'C' && str[j] == 'O' && str[k] == 'W') cnt++;
+            }
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
