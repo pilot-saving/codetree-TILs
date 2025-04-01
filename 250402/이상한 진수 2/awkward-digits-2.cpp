7321c5c8f64430835fb77790e0628a1fe43fed1f
Woo Yeonseo <106312178+pilot-saving@users.noreply.github.com>
2025-04-02 07:36:13 +0900

    Title: 이상한 진수 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250402/이상한 진수 2/awkward-digits-2.cpp
+++ 250402/이상한 진수 2/awkward-digits-2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstring>
 #include <bitset>
 
 using namespace std;
@@ -10,12 +9,16 @@ int main() {
     cin >> a;
 
     // Please write your code here.
+    bool changed = false;
     for (int i=0; i<a.length(); i++) {
         if (a[i] == '0') {
             a[i] = '1';
+            changed = true;
             break;
         }
     }
+
+    if (!changed) a[a.length()-1] = '0';
 
     bitset<10> bits(a);
     cout << bits.to_ulong();
